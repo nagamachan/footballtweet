9308bb78710d6883d0b2c279c5da0139e215b4ba
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-02-25 22:42:16 +0900

    Update tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,8 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
-  has_many :comments  # commentsテーブルとのアソシエーション
-  has_many :likes
+  has_many :comments, dependent: :destroy # commentsテーブルとのアソシエーション
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   def self.search(search)
